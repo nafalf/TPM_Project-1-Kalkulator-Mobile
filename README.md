5f3e4473df7b4256064a7eb26e45d0f8447c7828
unknown <rafiqikhwan16@gmail.com>
2025-02-19 18:48:55 +0700

    Your commit message

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # TPM_Project-1-Kalkulator-Mobile
 ## Tugas 2 Kelompok Teknologi Pemrograman Mobile 
 Membuat Kalkulator sederhana dengan beberapa fitur
@@ -8,3 +9,21 @@ Anggota:
 
 - Figma: https://www.figma.com/design/iePjjrjW9EiAyrSEkdF9mZ/Mobile-Teori?t=qdbUS54YJZWIoLlM-1
 - PPT  : 
+=======
+# tugas_2
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+>>>>>>> b45fc32 (main menu dan our team)
---
+++ lib/our_team.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class OurTeamPage extends StatelessWidget {
+  const OurTeamPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFF8E9F0), // Warna latar belakang
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              "OUR TEAM",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Expanded(
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    _buildTeamMember("assets/profile.jpg", "SYIFA NUR RAMADHANI", "123220194"),
+                    _buildTeamMember("assets/profile2.jpg", "M. NAUFAL FAUZI ALI", "123220207"),
+                  ],
+                ),
+                _buildTeamMember("assets/profile3.jpg", "RAFIQ IKHWAN", "123220"),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTeamMember(String imagePath, String name, String id) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          CircleAvatar(
+            radius: 50,
+            backgroundImage: AssetImage(imagePath),
+          ),
+          SizedBox(height: 10),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+            decoration: BoxDecoration(
+              color: Colors.purple.shade100,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Column(
+              children: [
+                Text(
+                  name,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Text(id),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
